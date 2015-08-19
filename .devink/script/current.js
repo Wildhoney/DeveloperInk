9964aee32edaf7da4c4abd37ec997b4d72f3008f
Wildhoney <adam.timberlake@gmail.com>
2015-08-19 11:38:10 +0100

    Moved scripts; added current script

--- .devink/script/current.js
+++ .devink/script/current.js
@@ -2,13 +2,9 @@
 
     const argv    = $minimist(process.argv.slice(2));
     const name    = argv._[0];
-    const path    = require('path').normalize(__dirname + '/../templates/' + name);
-    const current = require('path').normalize(__dirname + '/../templates/.current');
+    const path    = require('path').normalize(__dirname + '/../../templates/' + name);
+    const current = require('path').normalize(__dirname + '/../../templates/.current');
     const fs      = require('fs');
-
-    if (!fs.existsSync(path)) {
-        fs.mkdirSync(path);
-    }
 
     fs.unlinkSync(current);
     fs.symlinkSync(path, current, 'dir');
--- .devink/script/server.js
+++ .devink/script/server.js
@@ -6,7 +6,7 @@
     const app     = express();
     const server  = require('http').createServer(app);
     const port    = ($process.env.PORT || 5000);
-    const path    = require('path').normalize(__dirname + '/../templates/.current');
+    const path    = require('path').normalize(__dirname + '/../../templates/.current');
 
     app.use(express.static(path));
     server.listen(port);
