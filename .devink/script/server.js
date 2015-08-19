331944fb996819f298ffa3b4a5b87b3783f70b35
Wildhoney <adam.timberlake@gmail.com>
2015-08-19 12:32:29 +0100

    Fixed bug in server

--- .devink/script/server.js
+++ .devink/script/server.js
@@ -13,11 +13,11 @@
 
     const boilerplateIndex = fs.readFileSync(path.normalize(__dirname + '/../ink/1.0.5/boilerplate.html'), 'UTF-8');
     const boilerplateCss   = fs.readFileSync(path.normalize(__dirname + '/../ink/1.0.5/ink.css'), 'UTF-8');
-    const templateIndex    = fs.readFileSync(current + '/index.html', 'UTF-8');
 
     app.get('/', function(req, res) {
 
-        const template = Handlebars.compile(boilerplateIndex);
+        const templateIndex = fs.readFileSync(current + '/index.html', 'UTF-8');
+        const template      = Handlebars.compile(boilerplateIndex);
         res.send(template({ css: boilerplateCss, template: templateIndex }));
 
     });
